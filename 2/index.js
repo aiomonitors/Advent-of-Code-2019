52cb180f36437569b28ebd021b3679ff4e19483b
aiomonitors <navrxo@gmail.com>
2019-12-02 18:26:12 -0500

    Finished day 2

--- 2/index.js
+++ 2/index.js
@@ -1,32 +1,76 @@
 const fs = require('fs');
-const data = fs.readFileSync(__dirname + '/input.txt').toString().split(',').map(i => parseInt(i))
+const initialData = fs.readFileSync(__dirname + '/input.txt').toString().split(',').map(i => parseInt(i))
 
-let output = data
 //console.log(output)
-const comp = (data) => {
+const comp = async (data) => {
     for (let i  = 0; i < data.length; ++i) {
         //console.log(data[i])
         //console.log(i)
         if (data[i] == 1 || data[i] == 2) {
-            processOpCode(data.slice(i, i+4));
+            data = await processOpCode(data.slice(i, i+4));
             i = i+3
         } else if (data[i] == 99 ) {
             i = data.length
         }
     }
-    return (output);
+    return (data);
 }
 
-const processOpCode = (codeArr) => {
-    console.log(codeArr)
+const processOpCode = (codeArr, data) => {
+    //console.log(codeArr)
     if (codeArr[0] == 1) {
-        output[codeArr[3]] = data[codeArr[1]] + data[codeArr[2]];
-        console.log(output)
+        data[codeArr[3]] = data[codeArr[1]] + data[codeArr[2]];
+        //console.log(output)
     } else if (codeArr[0] == 2) {
-        output[codeArr[3]] = data[codeArr[1]] * data[codeArr[2]];
+        data[codeArr[3]] = data[codeArr[1]] * data[codeArr[2]];
     }
+
+    return data;
 }
 
-//processOpCode([1, 1, 1, 4])
+//part two
+const calculateGravityBoost = async (endResult) => {
+    try {
+        for (let x = 0; x <= 100; x++) {
+            for (let y = 0; y <= 100; y++) {
+                console.log(`Trying x: ${x}, y: ${y}`);
+                let answer = await comp2(initialData, x, y);
+                if (answer[0] === endResult) {
+                    console.log(`Output: ${answer[0]} - Noun: ${answer[1]} - Verb: ${answer[2]}`)
+                    exit();
+                }
+            }
+        } 
+    } catch (e) { throw(e)}
+    
+}
 
-console.log(comp(data))
+const comp2 = (initial, noun = 12, verb = 2) => {
+    let data = [...initial];
+    data[1] = noun;
+    data[2] = verb;
+    for (let i  = 0; i < data.length; ++i) {
+        //console.log(data[i])
+        //console.log(i)
+        if (data[i] == 1 || data[i] == 2) {
+            data = processOpCode(data.slice(i, i+4), data);
+            i = i+3
+        } else if (data[i] == 99 ) {
+            i = data.length
+        }
+    }
+    return (data);
+}
+const generateRandomNumbers = async (range) => {
+    let pairs = [];
+    for (let x = 0; x < range; x++) {
+        for(let y = 0; y < range; y++) {
+            pairs.push([x, y])
+        }
+    }
+    return pairs;
+}
+
+calculateGravityBoost(19690720).then(pair => {
+    console.log('done')
+});
